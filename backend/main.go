d401b8d6a374c6b1261afe3fa95e83cba1ad6965
Zohaib Butt <zohaib.butt@pearlgroup.no>
2021-09-22 19:57:30 +0200

    added procfile

--- backend/main.go
+++ backend/main.go
@@ -31,7 +31,7 @@ func main() {
 	log.Printf("%s",path);
 
 	router.HandleFunc("/api/v1/cv", controller.CVController{}.HandleCVForm).Methods("GET", "POST")
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir(frontendPath)))
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/dist/webapp/")))
     log.Println("Listing for requests at http://localhost:" + port + "/" + "\n");
 	log.Fatal(http.ListenAndServe(":"+port, router))
 
